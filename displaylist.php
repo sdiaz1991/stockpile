dfe4cd931d5770b34da841f797bbd4ca46ddf501
dcorey <dcorey.rasmussen@gmail.com>
2017-06-01 19:32:51 -0400

    instructor uncommented header
    instructor uncommented header

--- displaylist.php
+++ displaylist.php
@@ -1,31 +1,49 @@
-<?php
-$servername = "AZURE HOST NAME";
-$username = "YOUR_USERNAME";
-$password = "YOUR_PASSWORD";
-$dbname = "YOUR_DBNAME";
+<html>
+		<head>
+	<title>Find a Contact</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/custom.css" />
+<link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
+<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<script src="javascript/storage.js"></script>
+</head>
+	<body>
+		<div id="page" data-role="page" data-theme="b" >
+	<div data-role="header" data-theme="b">
+<h1>
+	Find your contact
+		</h1>	</div>
+				<div data-role="content">
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
 
-$sql= "SELECT id,fname, lname, email, comments FROM [YOUR_TABLE_NAME]";
-$result = mysqli_query($conn, $sql);
+					<?php
+					include 'config.php';
+					include 'opendb.php';
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. "<br>";
-        echo "Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
-        echo "Email: " . $row["email"]. "<br>";
-        echo "Comments: " . $row["comments"]."<br><hr>";
-    }
-} else {
-    echo "0 results";
-}
+					$sql= "SELECT id, fname, lname
+					FROM lecture1";
+					$result = mysqli_query($conn, $sql);
 
-mysqli_close($conn);
+					if (mysqli_num_rows($result) > 0) {
+					    // output data of each row
+					    while($row = mysqli_fetch_assoc($result)) {
+									echo "ID: " . $row["id"]. "<br>";
+					        echo "First Name: " . $row["fname"]. "<br>";
+					        echo "Last Name: " . $row["lname"]. "<br><hr>";
+					    }
+					} else {
+					    echo "0 results";
+					}
 
-?>
+					mysqli_close($conn);
+
+					?>
+
+				<div data-role="footer" data-theme="b">
+	  <h4>Darice Corey-Gilbert &copy; 2017</h4>
+	</div>
+	</body>
+</html>
