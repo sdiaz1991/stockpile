c94b48a2da6ad27c4c58699662b7ec1242315ab5
sdiaz1991 <Sireta_diaz1991@yahoo.com>
2017-06-02 20:07:31 -0400

    advance mobile app

--- find_a_record/findrecord.php
+++ find_a_record/findrecord.php
@@ -30,7 +30,7 @@ $dbname = "jackson156";
 
 // Create a connection
 
-$conn = mysqli_connect(us-cdbr-azure-central-a.cloudapp.net,bd5ab470b5c23b, 49de310d, jackson156);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass , $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
