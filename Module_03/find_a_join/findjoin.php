cd3c954ffe03394638959b069fbffa81d25ec059
dcorey <dcorey.rasmussen@gmail.com>
2017-06-08 22:32:48 -0400

    updated page
    updated page

--- Module_03/find_a_join/findjoin.php
+++ Module_03/find_a_join/findjoin.php
@@ -1,6 +1,6 @@
 <html>
 		<head>
-	<title>Find a record</title>
+	<title>Find a Contact</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="styles/custom.css" />
 <link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
@@ -13,45 +13,43 @@
 		<div id="page" data-role="page" data-theme="b" >
 	<div data-role="header" data-theme="b">
 <h1>
-	Find a record
+	Find your contact
 		</h1>	</div>
 				<div data-role="content">
-	<?php
-
-			include 'config.php';
-			include 'opendb.php';
 
 
-			$companyid = (isset($_POST['companyid'])    ? $_POST['companyid']   : '');
+					<?php
+					include 'config.php';
+					include 'opendb.php';
 
-			$sql= "SELECT company.companyid, company.address, company.city, company.zip, ordernumber.ordernumberid, ordernumber.dateofdelivery
-					FROM company
-					JOIN ordernumber on company.companyid = ordernumber.companyid2
-					WHERE companyid LIKE '$companyid' LIMIT 1";
+					$companyid = (isset($_POST['companyid'])    ? $_POST['companyid']   : '');
 
-			$result = mysqli_query($conn, $sql);
+					$sql= "SELECT company.companyid, company.address, company.city, company.zip, ordernumber.ordernumberid, ordernumber.dateofdelivery
+							FROM company
+							JOIN ordernumber on company.companyid = ordernumber.companyid2
+							WHERE companyid LIKE '$companyid' LIMIT 1";
+					$result = mysqli_query($conn, $sql);
 
-			if (mysqli_num_rows ($result) > 0) {
-				//output data of each row
-				while ($row = mysqli_fetch_assoc ($result)) {
-					echo "Company: " . $row["companyid"]. "<br>";
-					echo "address: " . $row["address"]. "<br>";
-					echo "city:    " . $row["city"]. "<br>";
-					echo "zip:     "  .$row["zip"]. "<br>";
-					echo "Ordernumber: " $row["ordernumberid"]. "<br>";
-					echo "dateofdelivery: " $row["dateofdelivery"]. "<br>";
+					if (mysqli_num_rows($result) > 0) {
+					    // output data of each row
+					    while($row = mysqli_fetch_assoc($result)) {
+									echo "CompanyID: " . $row["companyid"]. "<br>";
+					        echo "Address: " . $row["address"]. "<br>";
+					        echo "City: " . $row["city"]. "<br>";
+									echo "Zip: " . $row["zip"]. "<br>";
+									echo "Order #: " . $row["ordernumberid"]. "<br>";
+									echo "Date of Delivery: " . $row["dateofdelivery"]. "<br>";
+					    }
+					} else {
+					    echo "0 results";
 					}
-				} else {
-				    echo "0 results";
-				}
 
-				mysqli_close($conn);
+					mysqli_close($conn);
 
-		?>
-		</div>
-		</div>
+					?>
+
 				<div data-role="footer" data-theme="b">
-			  <h4>Sireta Diaz &copy; 2016</h4>
-			</div>
+	  <h4>Darice Corey-Gilbert &copy; 2017</h4>
+	</div>
 	</body>
 </html>
